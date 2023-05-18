adabffaea79c799a88def5769a0edf36bafac3df
Minedelamour49 <donelmichael412@gmail.com>
2023-05-18 05:11:57 -0700

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""
+Square matrix module
+"""
+
 
 def square_matrix_simple(matrix=[]):
-    """Computes the square value of all integers in a matrix"""
+    """
+    Computes the square value of all integers in a matrix
+    """
     new_matrix = []
     for row in matrix:
-        new_row = [num ** 2 for num in row]
+        new_row = [x ** 2 for x in row]
         new_matrix.append(new_row)
     return new_matrix
-
