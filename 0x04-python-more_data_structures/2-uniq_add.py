eef1413944ba3071a2026d24e78666c17086c058
Minedelamour49 <donelmichael412@gmail.com>
2023-05-18 05:22:58 -0700

    update

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+Unique addition module
+"""
+
+
 def uniq_add(my_list=[]):
-    uniq_list = set(my_list)
-    num = 0
-    for i in uniq_list:
-        num += i
-        return(num)
+    """
+    Adds all unique integers in a list
+    """
+    unique_nums = set(my_list)
+    result = sum(unique_nums)
+    return result
