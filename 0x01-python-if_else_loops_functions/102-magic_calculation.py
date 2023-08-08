de216cc5dfa53f9bfdbe33abe58ad9f64a1dadb9
Minedelamour49 <donelmichael412@gmail.com>
2023-08-08 12:13:57 -0700

    Python -If/else

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#102-magic_calculation.py
+
+def magic_calculations(a,b,c):
+    """Match bytecode providedby Holberton School."""
+    if a<b:
+        return (c)
+    ifc>b:
+        return (a,+b)
+    return (a*b - c)
