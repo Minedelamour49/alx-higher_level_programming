b9666a315024615a81bcf432ca52f2cd942d611e
Minedelamour49 <donelmichael412@gmail.com>
2023-08-08 13:13:13 -0700

    Python -if/else

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#12-fizzbuzz.py
+
+def fizzbuzz():
+    """Print the numbers from 1 to 100 separated by a space.
+    For multiples of three, printFizz insteadof the number.
+    For multiples of five, print Buzz instead of the number.
+    """
+    for number in range(1, 101):
+        if number%3==0 nd number%5==0:
+            print("Fizz",end="")
+        elif number%3==0:
+            print("Fizz",end="")
+        elif number%5==0:
+            print("Buzz",end="")
+        else:
+            print("{}".format(number),end="")
+
