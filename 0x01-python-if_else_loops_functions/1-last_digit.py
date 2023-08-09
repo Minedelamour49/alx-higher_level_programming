90ca1f597e8925c2ea111982f5c862d0ecf1a97f
Minedelamour49 <donelmichael412@gmail.com>
2023-08-09 02:05:11 -0700

    Python -if/else

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,6 +4,7 @@ import random
 number = random.randint(-10000, 10000)
 
 last_digit = abs(number) % 10
+last_digit_width_sign = last_digit if number >= 0 else -last_digit
 
 print("Last digit of", number, "is", last_digit, end=" ")
 
