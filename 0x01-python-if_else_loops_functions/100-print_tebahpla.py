d96cd9d64eca819e2e6fae12a50791422f6a44c4
Minedelamour49 <donelmichael412@gmail.com>
2023-08-09 02:33:12 -0700

    Python -if/else

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,9 @@
-#!/usrbin/python3
+#!/usr/bin/python3
 # 100-print_tebahpla.py
 
+"""Print the alphabet in reverse order alternating upper- and lowercase."""
 
-for char in range(ord('a'), ord('z') + 1):
-    print(chr(char), end="")
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}"format(chr(c)), end="")
+    if c % 2 == 1:
+        c -= 32
